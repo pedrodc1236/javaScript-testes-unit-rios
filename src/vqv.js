450121bc290fbcc87484acf28328734ac995e8c8
Pedro Tavares <pedrodc1236@gmail.com>
2022-01-17 17:04:25 -0300

    requisito 3 completo

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  let mensage = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  if (typeof name === 'string' && typeof age === 'number') {
+    return mensage;
+  } 
+    return undefined;
+};
 
 module.exports = vqv;
